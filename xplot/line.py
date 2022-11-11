426050ff4cdf0f34e8e4e7acb38b1586566fd11e
Philipp Niedermayer <eltos@outlook.de>
2022-11-11 08:46:21 +0100

    Handle thin elements and line=None

--- xplot/line.py
+++ xplot/line.py
@@ -28,6 +28,7 @@ class KnlPlot(XsuitePlot):
         filled=True,
         display_units=None,
         resolution=1000,
+        line_length=None,
         **subplots_kwargs,
     ):
         """
@@ -39,6 +40,7 @@ class KnlPlot(XsuitePlot):
         :param filled: If True, make a filled plot instead of a line plot.
         :param display_units: Dictionary with units for parameters. Supports prefix notation, e.g. 'bet' for 'betx' and 'bety'.
         :param resolution: Number of points to use for plot.
+        :param line_length: Length of line (only required if line is None).
         :param subplots_kwargs: Keyword arguments passed to matplotlib.pyplot.subplots command when a new figure is created.
 
         """
@@ -46,10 +48,14 @@ class KnlPlot(XsuitePlot):
 
         if knl is None:
             if line is None:
-                raise ValueError("Either line or N parameter must not be None")
-            knl = range(max([e.order for e in line.elements if hasattr(e, "order")]) + 1)
+                raise ValueError("Either line or knl parameter must not be None")
+            knl = range(
+                max([e.order for e in line.elements if hasattr(e, "order")]) + 1
+            )
         self.knl = knl
-        self.S = np.linspace(0, line.get_length(), resolution)
+        if line is None and line_length is None:
+            raise ValueError("Either line or line_length parameter must not be None")
+        self.S = np.linspace(0, line_length or line.get_length(), resolution)
         self.filled = filled
 
         # Create plot
@@ -64,10 +70,16 @@ class KnlPlot(XsuitePlot):
         for n in self.knl:
             if self.filled:
                 artist = self.ax.fill_between(
-                    self.S, np.zeros_like(self.S), alpha=0.5, label=self.label_for(f"k{n}l"), zorder=3
+                    self.S,
+                    np.zeros_like(self.S),
+                    alpha=0.5,
+                    label=self.label_for(f"k{n}l"),
+                    zorder=3,
                 )
             else:
-                (artist,) = self.ax.plot([], [], alpha=0.5, label=self.label_for(f"k{n}l"))
+                (artist,) = self.ax.plot(
+                    [], [], alpha=0.5, label=self.label_for(f"k{n}l")
+                )
             self.artists.append(artist)
         self.ax.plot(self.S, np.zeros_like(self.S), "k-", lw=1)
         self.ax.legend(ncol=5)
@@ -86,11 +98,15 @@ class KnlPlot(XsuitePlot):
         """
         # compute knl as function of s
         KNL = np.zeros((len(self.knl), self.S.size))
-        for s, name, el in zip(line.get_s_elements(), line.element_names, line.elements):
+        el_s0 = line.get_s_elements("upstream")
+        el_s1 = line.get_s_elements("downstream")
+        for name, el, s0, s1 in zip(line.element_names, line.elements, el_s0, el_s1):
+            if s0 == s1:  # thin lense located at element center
+                s0, s1 = (s0 + s1 - el.length) / 2, (s0 + s1 + el.length) / 2
             if hasattr(el, "knl"):
                 for i, n in enumerate(self.knl):
                     if n <= el.order:
-                        mask = (s <= self.S) & (self.S < s + el.length)
+                        mask = (s0 <= self.S) & (self.S < s1)
                         KNL[i, mask] += el.knl[n]
 
         # plot
@@ -99,15 +115,16 @@ class KnlPlot(XsuitePlot):
         for n, art, knl in zip(self.knl, self.artists, KNL):
             f = self.factor_for(f"k{n}l")
             if self.filled:
-                art.get_paths()[0].vertices[1 : 1 + knl.size, 1] = f * knl  # TODO shape 1003 ?
+                art.get_paths()[0].vertices[1 : 1 + knl.size, 1] = f * knl
             else:
                 art.set_data((s * self.S, f * knl))
             changed.append(art)
         if autoscale:
             if self.filled:  # At present, relim does not support collection instances.
-
                 self.ax.update_datalim(
-                    mpl.transforms.Bbox.union([a.get_datalim(self.ax.transData) for a in self.artists])
+                    mpl.transforms.Bbox.union(
+                        [a.get_datalim(self.ax.transData) for a in self.artists]
+                    )
                 )
             else:
                 self.ax.relim()
