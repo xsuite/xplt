f044b1593060e12e57706678355068d9cc606f43
Philipp Niedermayer <eltos@outlook.de>
2024-08-02 18:59:49 +0200

    Fix for timeseries=None

--- xplt/timestructure.py
+++ xplt/timestructure.py
@@ -1192,9 +1192,7 @@ class SpillQualityPlot(_TimeBasePlot, ParticlePlotMixin, MetricesMixin):
             Changed artists
         """
 
-        timeseries = self._ensure_particles_timeseries_data(
-            particles, timeseries, keys=["count"]
-        )["count"]
+        timeseries = self._ensure_particles_timeseries_data(particles, timeseries, keys=["count"])
 
         # Particle timestamp based data
         ################################
@@ -1211,6 +1209,7 @@ class SpillQualityPlot(_TimeBasePlot, ParticlePlotMixin, MetricesMixin):
         # Timeseries based data
         ########################
         else:
+            timeseries = timeseries["count"]
 
             # extract times in range
             if self.time_range is not None:
